3e76a424bb56105fa8da0a8d9ded880132f0665c
Md Ariful Islam <hi@imdariful.com>
2024-03-14 02:16:07 +0600

    enable cors

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+// enable cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  next();
+});
+
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
